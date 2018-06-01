05b480a8d717615c583c8016c0a506721df5843a
unknown <491854473@qq.com>
2018-06-01 20:10:25 +0800

    添加整个项目

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <letNav></letNav>
+    <div class="main">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+import letNav from '@/components/common/letNav'
+export default {
+  name: 'App',
+  components:{
+    letNav
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Microsoft YaHei','Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.main{
+  float: left;
+  width: 95%;
+  background-color: #eff2f7;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
